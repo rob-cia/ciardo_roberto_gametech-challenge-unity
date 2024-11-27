0eb333a84c59030f01523f51162fc5265ad5505f
rob-cia <roberto.ciardo@studenti.polito.it>
2024-11-27 19:09:45 +0100

    Implementation of drag and drop UI + logic drag and drop

---
+++ Assets/Scripts/DragAndDrop/Draggable.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+
+    [HideInInspector]
+    public Transform parentToReturnTo = null;
+    [HideInInspector]
+    public Transform placeHolderParent = null;
+
+    private GameObject placeHolder = null;
+
+    [Header("Prefab to instantiate")]
+    [SerializeField]
+    private DropZone dropZone = null;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        placeHolder = Instantiate(dropZone.placeHolder);//new GameObject();
+        placeHolder.transform.SetParent(this.transform.parent);
+        LayoutElement le = placeHolder.AddComponent<LayoutElement>();
+        le.preferredWidth = this.GetComponent<LayoutElement>().preferredWidth;
+        le.preferredHeight = this.GetComponent<LayoutElement>().preferredHeight;
+        le.flexibleWidth = 0;
+        le.flexibleHeight = 0;
+
+        placeHolder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
+
+        parentToReturnTo = this.transform.parent;
+        placeHolderParent = parentToReturnTo;
+        this.transform.SetParent(this.transform.parent.parent);
+
+        this.GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        this.transform.position = eventData.position;
+
+        if (placeHolder.transform.parent != placeHolderParent)
+        {
+            placeHolder.transform.SetParent(placeHolderParent);
+        }
+
+        int newSiblingIndex = placeHolderParent.childCount;
+
+        for (int i = 0; i < placeHolderParent.childCount; i++)
+        {
+            if (this.transform.position.y > placeHolderParent.GetChild(i).position.y)
+            {
+                newSiblingIndex = i;
+
+                if (placeHolder.transform.GetSiblingIndex() < newSiblingIndex)
+                    newSiblingIndex--;
+
+                break;
+            }
+        }
+
+        placeHolder.transform.SetSiblingIndex(newSiblingIndex);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        this.transform.SetParent(parentToReturnTo);
+        this.transform.SetSiblingIndex(placeHolder.transform.GetSiblingIndex());
+        this.GetComponent<CanvasGroup>().blocksRaycasts = true;
+        Destroy(placeHolder);
+    }
+}
